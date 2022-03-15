1bf042458a7f145b90c96a89d4b0b8f6f063b55e
marco maione <maionemarco14@gmail.com>
2022-03-15 14:27:59 +0100

    aggiornamento

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+class Movie {
+    public $genere;
+    public $durata;
+    public $titolo;
+    public $voto;
+
+    public function __construct($genere, $titolo) {
+        $this->genere = $genere;
+        $this->titolo = $titolo;
+    }
+}
+
+$batman = new Movie('Azione','Il Cavaliere Oscuro:');
+echo 'Titolo=> ' . $batman -> titolo . ' Genere=>' . $batman-> genere;
