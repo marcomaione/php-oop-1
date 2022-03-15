e986e1627aacab66daaeed77f1c649d85d8977c5
marco maione <maionemarco14@gmail.com>
2022-03-15 14:45:45 +0100

    aggiornamento

--- index.php
+++ index.php
@@ -10,7 +10,16 @@ class Movie {
         $this->genere = $genere;
         $this->titolo = $titolo;
     }
+
+    public function setVoto($voto) {
+
+        if (is_numeric($voto) && $voto > 0) {
+            $this->voto = $voto;
+        } else {
+            echo "Devi inserire un numero!";
+        }
 }
 
 $batman = new Movie('Azione','Il Cavaliere Oscuro:');
-echo 'Titolo=> ' . $batman -> titolo . ' Genere=>' . $batman-> genere;
+$batman->setVoto(10);
+echo 'Titolo = ' . $batman -> titolo . ' Genere = ' . $batman-> genere;
